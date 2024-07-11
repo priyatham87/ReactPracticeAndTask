4d5b3df3cc55739cd94ed58e288587e15dc07a6a
priyatham87 <saipriyatham016@gmail.com>
2024-07-11 13:10:54 +0530

    deleted all unwanted files and code

--- src/components/list/unorderlist.js
+++
@@ -1,9 +0,0 @@
-import ListIteams from "./listiteams";
-const UnOrderList=()=>{
-    return(
-        <ul>
-            <ListIteams></ListIteams>
-        </ul>
-    );
-}
-export default UnOrderList;
