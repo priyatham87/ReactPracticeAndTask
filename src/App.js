e3fc2d99133b4e03e87442ed4dad267b0a260876
priyatham87 <saipriyatham016@gmail.com>
2024-07-18 16:10:26 +0530

    July 17 Practice and Task

--- src/App.js
+++ src/App.js
@@ -2,33 +2,28 @@
 import './App.css';
 import Customcard from './components/card/card';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import fakeData, { Nav } from './components/fakedata/fakedata';
-import { Btn } from './components/button/button';
-import Subscribecomponent from './components/classComponent/subscribe-Button';
-import TodoComponent from './components/classComponent/todo';
-import Counter from './components/classComponent/counter';
-import Square from './components/classComponent/square-task';
-import FakeStoreApi from './components/classComponent/fakestoreapi';
+
+import FetchExample from './components/classComponent/fetchExample';
 
 
 
 function App() {
   return (
     <div>
-      <h1>16-7-24--(DAY-12 PRACTICE && TASK)</h1>
+      <h1>17-7-24--(DAY-13 PRACTICE && TASK)</h1>
       <center><h1>PRACTICE</h1></center>
-      <Subscribecomponent/>
-      <TodoComponent/>
+     
+     
+      <FetchExample/>
+    
+    
+      
      
       
 
       <center><h1>TASK</h1></center>
-      <Counter/>
-      <Square/>
-      
      
-
-      <FakeStoreApi/>
+      
 
     </div>
   );
