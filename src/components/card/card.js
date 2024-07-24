d5dcdcfbb6cab23c83c206fc1281f9447b47efa5
priyatham87 <saipriyatham016@gmail.com>
2024-07-24 12:25:30 +0530

    July 22 Practice and Task

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -10,16 +10,16 @@ class FetchData extends Component {
       loader: true,
       error: false,
       photos: [
-        "https://fakestoreapi.com/img/81fPKd-2AYL.AC_SL1500.jpg",
-        "https://fakestoreapi.com/img/71-3HjGNDUL.AC_SY879._SX._UX._SY._UY.jpg",
-        "https://fakestoreapi.com/img/71li-ujtlUL.AC_UX679.jpg",
-        "https://fakestoreapi.com/img/71YXzeOuslL.AC_UY879.jpg",
-        "https://fakestoreapi.com/img/71pWzhdJNwL.AC_UL640_QL65_ML3.jpg",
-        "https://fakestoreapi.com/img/61sbMiUnoGL.AC_UL640_QL65_ML3.jpg",
-        "https://fakestoreapi.com/img/71YAIFU48IL.AC_UL640_QL65_ML3.jpg",
-        "https://fakestoreapi.com/img/51UDEzMJVpL.AC_UL640_QL65_ML3.jpg",
-        "https://fakestoreapi.com/img/61IBBVJvSDL.AC_SY879.jpg",
-        "https://fakestoreapi.com/img/61U7T1koQqL.AC_SX679.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQin2nGaRBvBrhXhBb70_GXuWfd8F-ckQTVfg&s",
+        "https://www.wcmessenger.com/wp-content/uploads/2020/12/Willard-Ervin-Howell-obit-1.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMyFwdb_Kqpm-ME1WldFfvTba1CDsMUoAzVg&s",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMyFwdb_Kqpm-ME1WldFfvTba1CDsMUoAzVg&s",
+        "https://live.staticflickr.com/1720/24289969059_9caac89fd7_w.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQin2nGaRBvBrhXhBb70_GXuWfd8F-ckQTVfg&s",
+        "https://www.wcmessenger.com/wp-content/uploads/2020/12/Willard-Ervin-Howell-obit-1.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMyFwdb_Kqpm-ME1WldFfvTba1CDsMUoAzVg&s",
+        "https://live.staticflickr.com/1720/24289969059_9caac89fd7_w.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQin2nGaRBvBrhXhBb70_GXuWfd8F-ckQTVfg&s",
       ],
     };
   }
@@ -30,11 +30,13 @@ class FetchData extends Component {
         "https://jsonplaceholder.typicode.com/users"
       );
       const users = data;
+
       console.log(users);
       for (var i = 0; i < users.length; i++) {
-        users[i]["image"] = this.state.photos[i % this.state.photos.length];
+        users[i]["image"] = this.state.photos[i];
       }
 
+      console.log(users);
       if (status === 200) {
         this.setState({
           products: users,
@@ -49,8 +51,6 @@ class FetchData extends Component {
     }
   };
 
-  
-
   render() {
     return (
       <>
@@ -59,26 +59,23 @@ class FetchData extends Component {
           <h2>please wait</h2>
         ) : (
           <>
-          
-
-          <div className="flex">
-            {this.state.products.map((each) => {
-               
-              return (
-                <div key={each.id} className="card">
-                  <h1>{each.name}</h1>
-                  <img
-                    src={each.image}
-                    alt={each.name}
-                    heigth={200}
-                    width={150}
-                  />
-                  <h2>{each.username}</h2>
-                  <h3>{each.email}</h3>
-                </div>
-              );
-            })}
-          </div>
+            <div className="flex">
+              {this.state.products.map((each) => {
+                return (
+                  <div key={each.id} className="card">
+                    <h1>{each.name}</h1>
+                    <img
+                      src={each.image}
+                      alt={each.name}
+                      heigth={200}
+                      width={150}
+                    />
+                    <h2>{each.username}</h2>
+                    <h3>{each.email}</h3>
+                  </div>
+                );
+              })}
+            </div>
           </>
         )}
       </>
