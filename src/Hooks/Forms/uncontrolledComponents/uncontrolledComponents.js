d5e168b6fcd2210c153bbf1d8e5cc2df885ec588
priyatham87 <saipriyatham016@gmail.com>
2024-07-27 11:54:39 +0530

    July 25 Practice and Task

--- src/Hooks/Forms/uncontrolledComponents/uncontrolledComponents.js
+++ src/Hooks/Forms/uncontrolledComponents/uncontrolledComponents.js
@@ -1,86 +1,112 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from "react";
 
- function UncontrolledForms() {
-    const emailRef=useRef();
-    const passwordRef=useRef();
+function UncontrolledForms() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
-    const [error,setError]=useState("")
+  const [error, setError] = useState("");
 
-    const submitHandler=(event)=>{
-        event.preventDefault();
-        const emailEntered=emailRef.current.value;
-        const passwordEntered=passwordRef.current.value;
-        console.log("user entered data",emailEntered,passwordEntered);
+  const [userData,setUserData]=useState({})
 
-        if(emailEntered.length<15 && passwordEntered.length<15)
-        {
-            //allow user to submit
-            sucessSubmit(emailEntered, passwordEntered);
-            setError("")
-        }
-        else
-        {
-            //throw error
-            setError("please enter below 15 characters for email and password")
-        }
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const emailEntered = emailRef.current.value;
+    const passwordEntered = passwordRef.current.value;
+    console.log("user entered data", emailEntered, passwordEntered);
+
+    if (emailEntered.length < 15 && passwordEntered.length < 15) {
+      //allow user to submit
+      sucessSubmit(emailEntered, passwordEntered);
+      setError("");
+    } else {
+      //throw error
+      setError("please enter below 15 characters for email and password");
     }
+  };
 
-    const sucessSubmit=(username, password)=>{
-        fetch("https://dummyjson.com/auth/login", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              username: "emilys",
-              password: "emilyspass",
-            }),
-          })
-            .then((res) => res.json())
-            .then(data =>console.log(data));
-        alert("client validations are sucessfull")
-    }
+  const sucessSubmit = async (username, password) => {
+    // fetch("https://dummyjson.com/auth/login", {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify({
+    //       username: username,
+    //       password: password,
+    //     }),
+    //   })
+    //     .then((res) => res.json())
+    //     .then(data =>console.log(data));
+
+    try {
+      const response = await fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          // username: username,
+          // password: password,
+          username,
+          password,
+        }),
+      });
+      const finalResponse = await response.json();
+
+      if (finalResponse.message) {
+        alert(finalResponse.message);
+      } else {
+        console.log(finalResponse, "finalresponse");
+        setUserData(finalResponse)
+      }
+    } catch (err) {}
+    // alert("client validations are sucessfull")
+  };
   return (
-    <form onSubmit={submitHandler}>
-  <div className="mb-3 mt-3">
-    <label htmlFor="email" className="form-label">
-      Email:
-    </label>
-    <input
-      type="text"
-      className="form-control"
-      id="email"
-      placeholder="Enter email"
-      name="email"
-      ref={emailRef}
-    />
-  </div>
-  <div className="mb-3">
-    <label htmlFor="pwd" className="form-label">
-      Password:
-    </label>
-    <input
-      type="password"
-      className="form-control"
-      id="pwd"
-      placeholder="Enter password"
-      name="pswd"
-      ref={passwordRef}
-    />
-  </div>
-  {
-    error &&<span style={{color:"red"}}>{error}</span>
-  }
-  <br></br>
-  {/* <div className="form-check mb-3">
+    <>
+    {
+      Object.keys(userData).length>0 ?(
+        <>
+        <h2>Welcome {userData.firstName}</h2>
+        </>
+      ):<form onSubmit={submitHandler}>
+      <div className="mb-3 mt-3">
+        <label htmlFor="email" className="form-label">
+          Email:
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="email"
+          placeholder="Enter email"
+          name="email"
+          ref={emailRef}
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="pwd" className="form-label">
+          Password:
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="pwd"
+          placeholder="Enter password"
+          name="pswd"
+          ref={passwordRef}
+        />
+      </div>
+      {error && <span style={{ color: "red" }}>{error}</span>}
+      <br></br>
+      {/* <div className="form-check mb-3">
     <label className="form-check-label">
       <input className="form-check-input" type="checkbox" name="remember" />{" "}
       Remember me
     </label>
   </div> */}
-  <button type="submit" className="btn btn-primary">
-    Submit
-  </button>
-</form>
-
-  )
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+    }
+    </>
+    
+  );
 }
-export default UncontrolledForms
+export default UncontrolledForms;
