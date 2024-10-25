84caf4bd41e9d833d52c6462bd002d1e47eab376
priyatham87 <saipriyatham016@gmail.com>
2024-10-25 23:34:20 +0530

    task

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Search } from "./shoppers/search";
-import { Categories } from "./shoppers/categories";
 import { Navigations } from "./navigations/navigations";
 import { Todo } from "./todo/todo";
 
@@ -9,10 +8,9 @@ import { Todo } from "./todo/todo";
 function App() {
   return(
     <div>
-      <Search/>
-      {/* <Categories/> */}
-      <Navigations/>
-      {/* <Todo/> */}
+      {/* <Search/>
+      <Navigations/> */}
+      <Todo/>
     </div>
   )
 }
--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -1,38 +1,33 @@
-import React from 'react'
-import {NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav >
-    <div >
-      <ol style={{display:'flex', justifyContent:'space-between', width:500}}>
-        <li >
-          <NavLink to={"/men"} >
-           men
-          </NavLink>
-        </li>
-       
-          
-        <li className="nav-item start">
-          <NavLink to={"/women"} >
-          women
-          </NavLink>
-        </li>
+    <nav>
+      <div>
+        <ol
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            width: 500,
+          }}
+        >
+          <li>
+            <NavLink to={"/"}>men</NavLink>
+          </li>
 
-        <li className="nav-item start">
-          <NavLink to={"/kids"} >
-          kids
-          </NavLink>
-        </li>
-        <li className="nav-item start">
-          <NavLink to={"/Beauty"} >
-          Beauty
-          </NavLink>
-        </li>
-        
-      </ol> 
-    </div>
-  </nav>
-  
-  )
-}
+          <li>
+            <NavLink to={"/women"}>women</NavLink>
+          </li>
+
+          <li>
+            <NavLink to={"/kids"}>kids</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/Beauty"}>Beauty</NavLink>
+          </li>
+        </ol>
+      </div>
+    </nav>
+  );
+};
